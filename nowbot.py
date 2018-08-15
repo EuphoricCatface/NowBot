9890d501ce38ea5a90474eb5d4f784012a08cccb
EuphoricCatface <euphoriccatface@gmail.com>
2018-08-15 21:01:57 +0900

    got default handle_commands to run

--- nowbot.py
+++ nowbot.py
@@ -8,7 +8,11 @@
 import os
 import basebot
 
-class NowBot(basebot.BaseBot):
+def now_handler(self, cmdline, meta):
+    if len(cmdline) == 1:
+        meta['reply']('imgs.xkcd.com/comics/now.png')
+
+class NowBot(basebot.Bot):
     BOTNAME = 'NowBot'
     NICKNAME = 'LilNowBot'
     SHORT_HELP = 'Someday, I will grow up to be @NowBot, with even more commands!'
@@ -16,16 +20,12 @@ class NowBot(basebot.BaseBot):
                 'Someday, I will grow up to be @NotBot, with even more commands!'
 
     def __init__(self, *args, **kwds):
-        basebot.BaseBot.__init__(self, *args, **kwds)
-        SHORT_HELP = 'Someday, I will grow up to be @NowBot, with even more commands!'
-        LONG_HELP = 'I show xkcd.com/now on the command "!now"\n'+\
-                    'Someday, I will grow up to be @NotBot, with even more commands!'
+        basebot.Bot.__init__(self, *args, **kwds)
 
     def handle_command(self, cmdline, meta):
-        if cmdline[0] != '!now':
-            return
-        if len(cmdline) == 1:
-            meta['reply']('imgs.xkcd.com/comics/now.png')
+        if cmdline[0] == '!now':
+            now_handler(self, cmdline, meta)
+        basebot.Bot.handle_command(self, cmdline, meta)
 
 def main():
     basebot.run_main(NowBot)
