b4461e637ff7d737b4d8f8a5aceaca311f7aaa82
EuphoricCatface <euphoriccatface@gmail.com>
2018-08-18 00:02:07 +0900

    delete undeeded "self"

--- nowbot.py
+++ nowbot.py
@@ -24,7 +24,7 @@ class NowBot(basebot.Bot):
 
     def handle_command(self, cmdline, meta):
         if cmdline[0] == '!now':
-            now_handler(self, cmdline, meta)
+            now_handler(cmdline, meta)
         basebot.Bot.handle_command(self, cmdline, meta)
 
 def main():
